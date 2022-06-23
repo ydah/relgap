424010c0cea62a5a0a4baf387d31b5a549dd9960
ydah <13041216+ydah@users.noreply.github.com>
2022-06-23 10:00:21 +0900

    Update .rubocop.yml

--- spec/relgap_spec.rb
+++ spec/relgap_spec.rb
@@ -2,10 +2,10 @@
 
 RSpec.describe Relgap do
   it "has a version number" do
-    expect(Relgap::VERSION).not_to be nil
+    expect(Relgap::VERSION).not_to be_nil
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(false).to be(true)
   end
 end
